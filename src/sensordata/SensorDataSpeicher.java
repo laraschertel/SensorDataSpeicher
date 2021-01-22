d548d4e0ba935ee497462a5a56d15ff33a73afb4
lrnbr21 <laraschertel@gmail.com>
2021-01-22 19:32:48 +0100

    Implemented

--- src/sensordata/SensorDataSpeicher.java
+++ src/sensordata/SensorDataSpeicher.java
@@ -1,5 +1,7 @@
 package sensordata;
 
+import Exceptions.SensorException;
+
 import java.util.LinkedList;
 
 public interface SensorDataSpeicher {
@@ -7,18 +9,20 @@ public interface SensorDataSpeicher {
      *
      * @return average from one sensor
      */
-    public double getAverageOneSensor(LinkedList<Double> sensorValuesList);
+    float getAverageOneSensor(LinkedList<SensorData> sensorValuesList, String sensorName) throws SensorException;
 
     /**
      * @return average from all the sensors
      */
-    public double getAverageAllSensors(LinkedList<LinkedList<Double>> allSensors);
+    float getAverageAllSensors(LinkedList<SensorData> sensorValuesList) throws SensorException;
 
     /**
      *
      * @return the amount of measurements from a sensor
      */
-    public int getAmountOfMeasuresOneSensor(LinkedList<Double> sensorValuesList);
+    int getAmountOfMeasuresOneSensor(LinkedList<SensorData> sensorValuesList, String sensorName) throws SensorException;
+
+
 
 
 
--- src/sensordata/SensorDataSpeicherImpl.java
+++ src/sensordata/SensorDataSpeicherImpl.java
@@ -1,24 +1,59 @@
 package sensordata;
 
 
+import Exceptions.SensorException;
+import app.TCPSensorDataExchanger;
+
 import java.util.LinkedList;
 
-public class SensorDataSpeicherImpl implements SensorDataSpeicher{
-
+public class SensorDataSpeicherImpl implements SensorDataSpeicher {
 
     @Override
-    public int getAmountOfMeasuresOneSensor(LinkedList<Double> sensorValuesList) {
-        return 0;
+    public float getAverageOneSensor(LinkedList<SensorData> sensorValuesList, String sensorName) throws SensorException {
+        float sumSensorValues = 0;
+        int amountOfMeasures = 0;
+        for (int i = 0; i < sensorValuesList.size(); i++) {
+            if (sensorValuesList.get(i).getSensorName() == sensorName) {
+                sumSensorValues += sensorValuesList.get(i).getValue();
+                amountOfMeasures++;
+            }
+        }
+
+        if(amountOfMeasures == 0){
+            throw new SensorException("This sensor is not on the list");
+        }else {
+
+            return sumSensorValues / (amountOfMeasures);
+        }
     }
 
-
     @Override
-    public double getAverageOneSensor(LinkedList<Double> sensorValuesList) {
-        return 0;
+    public float getAverageAllSensors(LinkedList<SensorData> sensorValuesList) throws SensorException {
+        float sumSensorValues = 0;
+        for (int i = 0; i < sensorValuesList.size(); i++) {
+            sumSensorValues += sensorValuesList.get(i).getValue();
+        }
+        if(sensorValuesList.size() == 0){
+            throw new SensorException("The sensor list is empty, cannot calculate average");
+        }
+        return sumSensorValues / (sensorValuesList.size());
     }
 
     @Override
-    public double getAverageAllSensors(LinkedList<LinkedList<Double>> allSensors) {
-        return 0;
+    public int getAmountOfMeasuresOneSensor(LinkedList<SensorData> sensorValuesList, String sensorName) throws SensorException {
+        int amountOfMeasures = 0;
+        for (int i = 0; i < sensorValuesList.size(); i++) {
+            if (sensorValuesList.get(i).getSensorName() == sensorName) {
+                amountOfMeasures++;
+            }
+        }
+        if (amountOfMeasures == 0) {
+            throw new SensorException("This sensor is not on the list");
+        } else {
+
+            return amountOfMeasures;
+        }
     }
 }
+
+
